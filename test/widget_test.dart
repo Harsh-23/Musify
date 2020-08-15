ea779db4a9d382388449ec7aec0ae13bd6a029e8
Dhruvan Bhalara <dhruvan2912@yahoo.com>
2020-08-15 22:49:40 +0530

    Provide home class in test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:music/main.dart';
+import 'package:music/ui/musify.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(Musify());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
